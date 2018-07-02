688221d571b33019605f49663cc5e0971ae3b5ea
AhmedAbdelghffar <ahmedsoud3@gmail.com>
2018-07-02 03:12:55 -0700

    adding unyson

---
+++ framework-customizations/theme/options/settings.php
@@ -0,0 +1,11 @@
+<?php
+$options = array(
+    'option_id' => array(
+        'type'  => 'text',
+        'value' => 'Default value',
+        'label' => __('Option Label', '{domain}'),
+        'desc'  => __('Option Description', '{domain}'),
+        'attr'  => array('class' => 'custom-class', 'data-foo' => 'bar'),
+        'help'  => __('Some html that will appear in tip popup', '{domain}'),
+    )
+);
